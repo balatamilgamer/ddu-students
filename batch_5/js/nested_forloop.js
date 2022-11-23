0aa8b4901c6fb9aa30b8eda0583ef5dcde5a6ebc
balatamilgamer <balatamilgamer@gmail.com>
2022-11-23 11:29:38 +0530

    ajax

---
+++ batch_5/js/nested_forloop.js
@@ -0,0 +1,12 @@
+
+
+for(var i = 0; i <= 10; i++) {
+
+    var output = "";
+    for(var j = 0; j <= i ; j++) {
+        //console.log(j);
+        output += j + " ";
+    }
+
+    console.log(output);
+}
